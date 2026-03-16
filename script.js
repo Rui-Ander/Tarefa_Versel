4171ce4623806aa07f624d68c40279fae1e3d067
Nome <Email>
2026-03-16 14:35:51 -0300

    V1.3.1 - Correção para números grandes/ Alteração do nome

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let valor = 0;
 
+function atualizarFonte(){
+
+let contador = document.getElementById("contador");
+
+let tamanho = 70;
+
+contador.style.fontSize = tamanho + "px";
+
+while(contador.scrollWidth > contador.clientWidth && tamanho > 30){
+
+tamanho--;
+
+contador.style.fontSize = tamanho + "px";
+
+}
+
+}
+
 function incrementar(){
 
 let passo = parseInt(document.getElementById("passo").value);
@@ -12,6 +30,8 @@ valor += passo;
 
 document.getElementById("contador").textContent = valor;
 
+atualizarFonte();
+
 }
 
 function resetar(){
@@ -19,5 +39,7 @@ function resetar(){
 valor = 0;
 
 document.getElementById("contador").textContent = valor;
+
+document.getElementById("contador").style.fontSize = "70px";
 
 }
